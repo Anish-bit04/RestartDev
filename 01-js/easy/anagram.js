d3545e27bf010bf22685a1a6776d90f19cd07f24
Anish <Anish@lamarr.tech>
2024-06-15 02:11:18 +0530

    two question done in easy , one ques. don;t know

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,15 @@
 */
 
 function isAnagram(str1, str2) {
+    let l1 = str1.length
+    let l2 = str2.length
+    if(l1 !==l2) return false;
+   
+    s1 = str1.toLowerCase().split('').sort().join('')
+    s2 = str2.toLowerCase().split('').sort().join('')
+    return s1 == s2
+   
 
 }
-
+// console.log(isAnagram('asda','asda'))
 module.exports = isAnagram;
--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,15 @@
 */
 
 function findLargestElement(numbers) {
-    
+  let l1 = numbers.length;
+  let maxValue = numbers[0];
+  for (let i = 1; i < l1; i++) {
+    if (numbers[i] > maxValue) {
+      maxValue = numbers[i];
+    }
+  }
+  return maxValue;
 }
 
+// console.log(findLargestElement([3, 7, 2, 9, 1]));
 module.exports = findLargestElement;
